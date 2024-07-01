5c9b505069f55aab1fc8cf3cf0a4e2b33a25a4ca
VilaxDev <vilax.dev@gmail.com>
2024-07-01 11:56:17 -0500

    commit preview

---
+++ android/app/src/main/kotlin/com/example/xis/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.xis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:xis/pages/authentication/login_page.dart';
+import 'pages/chat_page.dart';
+import 'services/auth_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AuthChecker(),
+    );
+  }
+}
+
+class AuthChecker extends StatelessWidget {
+  final AuthService _authService = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: _authService.authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasData) {
+          return ChatPage();
+        }
+        return LoginPage();
+      },
+    );
+  }
+}
