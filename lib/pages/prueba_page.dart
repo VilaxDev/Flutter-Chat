a9aedf1b67474b3ba479580085cc3cb41f7fb7e5
VilaxDev <vilax.dev@gmail.com>
2024-07-01 14:54:41 -0500

    Exclude apis.dart

---
+++ lib/pages/prueba_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PruebaPage extends StatelessWidget {
+  const PruebaPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
